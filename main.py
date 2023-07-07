9944dce38989a1b63beb0570f81b79e52fb1776e
Kaothara Morakinyo <kmorakinyo2021@gmail.com>
2023-07-07 20:44:27 +0000

    changes to main

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+import git
+from flask import Flask, render_template, url_for, flash, redirect, request
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/kaothara23/kaothara23')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
